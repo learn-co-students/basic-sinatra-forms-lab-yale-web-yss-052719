489dd8555872e5d6d8b6e7909cb3a22879e4a1eb
MatthewP01 <matthew.peoples@yale.edu>
2019-06-18 16:29:58 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,18 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    @name = params[:name]
+    @pg = params[:pg]
+    @coach = params[:coach]
+    @pf = params[:pf]
+    @sg = params[:sg]
+    @c = params[:c]
+    erb :team
+  end
 
 end
